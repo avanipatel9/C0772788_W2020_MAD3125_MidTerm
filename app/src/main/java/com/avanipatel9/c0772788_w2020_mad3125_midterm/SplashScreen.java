390f43c4b0a5696e1239ffe2cbbdc8cb789a3e93
Avani Patel <anpatel36@yahoo.in>
2020-04-09 12:32:36 -0400

    [AP] theme changed and splash screen created

--- app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_midterm/SplashScreen.java
+++ app/src/main/java/com/avanipatel9/c0772788_w2020_mad3125_midterm/SplashScreen.java
@@ -1,10 +1,14 @@
 package com.avanipatel9.c0772788_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,6 +18,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Window window = getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(Color.WHITE);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
